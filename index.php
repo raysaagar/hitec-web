5edbdbb5ef10eb1000b898ef9470d934e7a8b8cd
Saagar Deshpande <sdeshpande@college.harvard.edu>
2013-09-28 17:31:16 -0400

    initial commit of website

---
+++ index.php
@@ -0,0 +1,226 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="assets/img/favicon.png">
+
+    <title>HITEC - Harvard Innovation, Technology, and Entrepreneurship Collaboration</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Custom styles for this template -->
+    <link href="bootstrap/css/jumbotron.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+
+    <!--Google fonts styles -->
+    <link href='http://fonts.googleapis.com/css?family=News+Cycle:400,700' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="bootstrap/js/html5shiv.js"></script>
+      <script src="bootstrap/js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body data-spy="scroll" data-target="#navbar-scroller" data-offset="70">
+
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+
+          <a class="navbar-brand" href="#">HITEC</a>
+        </div>
+        <div class="navbar-collapse collapse" id="navbar-scroller">
+          <ul class="nav navbar-nav" >
+            <li class="active"><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#events">Events</a></li>
+            <li><a href="#sponsors">Sponsors</a></li>
+          </ul>
+        </div><!--/.navbar-collapse -->
+      </div>
+    </div>
+
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron" id="home">
+      <!--<div class="container">
+        <h1>Hello, world!</h1>
+        <p><a class="btn btn-primary btn-lg">Learn more &raquo;</a></p>
+      </div>
+     -->
+      <div class = "splash">
+      	<h1 class="nophone">Bringing TEC to a new level.</h1>
+      	<h3>HITEC</h3>
+      	<h4>Innovation ▪ Technology ▪ Entrepreneurship</h4>
+      </div>
+    </div>
+
+    <div class="container feature" id = "about">
+      <!-- Example row of columns -->
+      <div class="row splash">
+      	<h3>HITEC is the largest undergraduate technology and entrepreneurship collaboration at Harvard</h3>
+      </div>
+      <p>Organized by HCS, D4D, HCVP, Hack Harvard, HCEF, DLP, and others, HITEC aims to // do things. //</p>
+
+      <!--
+      <div class="row splash">
+      	<div class="col-xs-6 col-md-6 col-lg-6">
+      		<img src="assets/img/d4d.jpg" class="img-responsive" alt="Developers for Development">
+      	</div>
+      	<div class="col-xs-6 col-md-6 col-lg-6">
+      		<img src="assets/img/gpi.jpg" class="img-responsive" alt="Developers for Development">
+      	</div>
+      </div>
+      -->
+
+      <div class="row topmargin">
+        <div class="col-xs-12 col-md-6 col-lg-6">
+          <h4>Member Organizations</h4>
+          <h5><a href="www.developersfordevelopment.org">Developers for Development</a></h5>
+          <div class="tabbed">
+            <h6>Use technology to make a difference in the world.</h6>
+          </div>
+
+          <h5><a href="www.hackharvard.org">HackHarvard</a></h5>
+          <div class="tabbed">
+            <h6>Bringing the world of tech entrepreneurship to Harvard.</h6>
+          </div>
+          <h5><a href="http://www.harvardentrepreneurshipforum.com/">Harvard College Entrepreneurship Forum
+</a></h5>
+          <div class="tabbed">
+            <h6>Bringing in the next class of entrepreneurs.</h6>
+          </div>
+
+          <h5><a href="www.hcs.harvard.edu">Harvard Computer Society</a></h5>
+          <div class="tabbed">
+            <h6>People. Technology. Awesome.</h6>
+          </div>
+
+        </div>
+        <div class="col-xs-12 col-md-6 col-lg-6">
+          <h4>&nbsp;</h4>
+
+          <h5><a href="www.dlp.io">Digital Literacy Project
+</a></h5>
+          <h6 class="tabbed">Expanding access to technology one community at a time.</h6>
+
+          <h5><a href="www.hcs.harvard.edu/hces">Harvard College Engineering Society</a></h5>
+          <h6 class="tabbed">Engineering at its finest.</h6>
+
+          <h5><a href="www.harvardventures.org">Harvard College Venture Partners</a></h5>
+          <div class="tabbed">
+            <h6>Launching student entrepreneurs and fostering collaboration.</h6>
+          </div>
+
+          <h5><a href="www.hcs.harvard.edu/~harvardwomenincs/">Women in Computer Science</a></h5>
+          <div class="tabbed">
+            <h6>Creating a network of women in technology.</h6>
+          </div>
+
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-md-12 col-lg-12">
+        </div>
+      </div>
+    </div> <!-- /container  end about -->
+
+<!--
+     <div class="container feature" id = "sponsors">
+
+      <div class="row splash">
+      	<h3>Sponsors</h3>
+      	<h5>We are currently looking for sponsors for ID Hack 2014.  If you or your company is interested, please contact us at <a href="mailto:directors.idhack@gmail.com">directors.idhack@gmail.com</a>.</h5>
+      </div>
+      <div class="row splash">
+      	<h4>Sponsorship Information</h4>
+        <p>Information about sponsorship can be found in our <a href="assets/img/ID Hack 2014 Sponsorship Letter.pdf"> sponsorship request leter.</a></p>
+      </div>
+      <div class="row splash" style="margin-top:30px;">
+      	<h4>Past Sponsors</h4>
+      </div>
+      <h4 class="sponsortitle" style="margin-top:0px;">Gold Sponsors </h4>
+      <div class="row splash">
+      	<div class="col-xs-6 col-md-6 col-lg-3">
+      		<img src="assets/img/intuit.jpg" class="img-responsive" alt="Intuit">
+      	</div>
+      	<div class="col-xs-6 col-md-6 col-lg-3">
+      		<img src="assets/img/microsoft.jpg" class="img-responsive" alt="Microsoft">
+      	</div>
+      	<div class="col-xs-6 col-md-6 col-lg-3">
+      		<img src="assets/img/ahealth.jpg" class="img-responsive" alt="athenaHealth">
+      	</div>
+      	<div class="col-xs-6 col-md-6 col-lg-3">
+      		<img src="assets/img/google.jpg" class="img-responsive" alt="Google">
+      	</div>
+      </div>
+      <h4 class="sponsortitle">Silver Sponsors</h4>
+      <div class="row splash">
+      	<div class="col-xs-4 col-md-4 col-lg-4">
+      		<img src="assets/img/jana.jpg" class="img-responsive" alt="Jana Care">
+      	</div>
+      	<div class="col-xs-4 col-md-4 col-lg-4">
+      		<img src="assets/img/healthbox.jpg" class="img-responsive" alt="HealthBox">
+      	</div>
+      	<div class="col-xs-4 col-md-4 col-lg-4">
+      		<img src="assets/img/oracle.jpg" class="img-responsive" alt="Oracle">
+      	</div>
+      </div>
+      <div class="row splash">
+      	<div class="col-xs-4 col-md-4 col-lg-4">
+      		<img src="assets/img/bridgewater.jpg" class="img-responsive" alt="Bridgewater Associates">
+      	</div>
+      	<div class="col-xs-4 col-md-4 col-lg-4">
+      		<img src="assets/img/endless.jpg" class="img-responsive" id="endless" alt="Endless Mobile">
+      	</div>
+      	<div class="col-xs-4 col-md-4 col-lg-4">
+      		<img src="assets/img/dmc.jpg" class="img-responsive" alt="DMC Consultants">
+      	</div>
+      </div>
+
+      <h4 class="sponsortitle">Bronze Sponsors</h4>
+      <div class="row splash">
+      	<div class="col-xs-2 col-md-2 col-lg-2">
+      		<img src="assets/img/cloudmine.jpg" class="img-responsive" alt="cloudmine">
+      	</div>
+      	<div class="col-xs-2 col-md-2 col-lg-2">
+      		<img src="assets/img/twilio.jpg" class="img-responsive" alt="Twilio">
+      	</div>
+      	<div class="col-xs-2 col-md-2 col-lg-2">
+      		<img src="assets/img/dropbox.jpg" class="img-responsive" alt="Dropbox">
+      	</div>
+      	<div class="col-xs-2 col-md-2 col-lg-2">
+      		<img src="assets/img/boloco.jpg" class="img-responsive" alt="Boloco">
+      	</div>
+      	<div class="col-xs-2 col-md-2 col-lg-2">
+      		<img src="assets/img/baker.jpg" class="img-responsive" alt="Baker Foundation">
+      	</div>
+      	<div class="col-xs-2 col-md-2 col-lg-2">
+      		<img src="assets/img/venture.png" class="img-responsive" alt="VenturePact">
+      	</div>
+      </div>
+-->
+      <hr>
+	<div class="container">
+      <footer>
+        <p>&copy; Harvard Innovation, Technology, and Entrepreneurship Collaboration 2013</p>
+      </footer>
+	</div>
+
+
+  </body>
+</html>
